c6c1a860ef7bc7a0807f5109b39b5fa44e94ab0e
Tim <bogackintimofey@gmail.com>
2026-09-15 19:19:07 +0300

    Обновлен статус задания № 51 Тема: 5 > Неверно

---
+++ Тема 5/Задания/-Задание 51.py
@@ -0,0 +1,17 @@
+# Решение
+#i = 427
+#N = bin(i)[2:-5]
+#print(N)
+#print(int(N, 2))
+#for i in range(1111111110, 1444444416):
+print(bin(34722222))
+print(bin(45138888))
+print(int(str(10000100011101000110101110), 2))
+print(int(str(10101100001100001111001000), 2))
+answer = 10416666
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(5, 51, answer, '389499b02f30212486e408cd73a5bc50'))
