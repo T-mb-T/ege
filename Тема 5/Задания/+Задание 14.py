25faa25cc58ade3ce31df8271014d8e15ba08247
Tim <bogackintimofey@gmail.com>
2026-09-15 17:21:26 +0300

    Обновлен статус задания № 14 Тема: 5 > Верно

---
+++ Тема 5/Задания/+Задание 14.py
@@ -0,0 +1,21 @@
+n = []
+for i in range(1, 150):
+    r = bin(i)[2:]
+    #11. 10. 1. 0
+    r = r + str(bin(i % 4)[2:])
+    print(int(r, 2), i)
+    n.append(int(r, 2))
+print(sorted(n))
+print(len(n))
+m = []
+for i in range(148):
+    m.append(sorted(n)[i + 1] - sorted(n)[i])
+print(m)
+
+answer = 19
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(5, 14, answer, '1f0e3dad99908345f7439f8ffabdffc4'))
