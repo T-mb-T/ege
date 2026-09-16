9b397f331a8913b8468af13e9d4788c672c72d3f
Tim <bogackintimofey@gmail.com>
2026-09-16 15:02:56 +0300

    Обновлен статус задания № 51 Тема: 5 > Верно

---
+++ Тема 5/Задания/+Задание 51.py
@@ -0,0 +1,21 @@
+# Решение
+#i = 427
+#N = bin(i)[2:-5]
+#print(N)
+#print(int(N, 2))
+#x = 0
+#for i in range(1111111110, 1444444417):
+#    if i % 32 == 1:
+#        x += 1
+#print(x)
+#print(int(str(10000100011101000110101110), 2))
+#print(int(str(10101100001100001111001000), 2))
+#x = int(str(10101100001100001111001000), 2) - int(str(10000100011101000110101110), 2)
+#print(x)
+answer = 10416665
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(5, 51, answer, '389499b02f30212486e408cd73a5bc50'))
