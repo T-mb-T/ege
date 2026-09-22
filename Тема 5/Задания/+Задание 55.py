4087502ff4c6593e239d821cd61841cd942411d3
Tim <bogackintimofey@gmail.com>
2026-09-22 15:23:40 +0300

    Обновлен статус задания № 55 Тема: 5 > Верно

---
+++ Тема 5/Задания/+Задание 55.py
@@ -0,0 +1,26 @@
+# Решение
+from numpy import base_repr
+x = []
+for i in range(1, 2000):
+    N = base_repr(i, 3)
+    print(N, i)
+    if N[-1] == '0':
+        N = N + N[-2:]
+    else:
+        N = N + base_repr(int(N[-1]) * 5, 3)
+    R = int(N, 3)
+    x += [R]
+    print(R, N, i)
+print(sorted(x))
+
+
+
+
+
+answer = 162
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(5, 55, answer, '82aa4b0af34c2313a562076992e50aa3'))
